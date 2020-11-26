cc19a0d11a568e1983e68f07254885f8a0527ba3
unknown <aromero@AROMERO.capaco.mcy>
2020-11-26 12:02:12 -0400

    Ready Card-Generator

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   let pinta = generateRadomSuit();
+  let numero = generateRadomNumber();
   document.querySelector(".top-suit").classList.add(pinta);
   document.querySelector(".top-suit").innerHTML = pinta;
+  document.querySelector(".number").innerHTML = numero;
   document.querySelector(".bottom-suit").classList.add(pinta);
   document.querySelector(".bottom-suit").innerHTML = pinta;
 };
@@ -34,6 +36,15 @@ let generateRadomNumber = () => {
 };
 let generateRadomSuit = () => {
   let suit = ["♠", "♣", "♥", "♦"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
+  let color = ["rojo", "negro"];
+  let indexSuit;
+  let colorSelect = color[Math.floor(Math.random() * color.length)];
+  if (colorSelect == "rojo") {
+    indexSuit = Math.floor(Math.random() * (suit.length - 2) + 2);
+    document.body.style.color = "red";
+  } else {
+    indexSuit = Math.floor(Math.random() * 2);
+    document.body.style.color = "black";
+  }
   return suit[indexSuit];
 };
